cccd7e372df3888a7de6cc1b8643e99206bc2ca3
hasnain-cedcoss <syedhasnainmehadi@cedcommerce.com>
2022-12-06 18:35:34 +0530

    organize env

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import "./App.css";
-import logo from "./logo.svg";
+import logo from "./src/assets/icons/logo.svg";
 
 function App() {
+  console.log("ENV", process.env.REACT_APP_ENV);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
